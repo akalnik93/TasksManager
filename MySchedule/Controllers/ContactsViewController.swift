98e739d277babe921901417837ce17b59b52315a
Алексей Калнык <aleksejkalnyk@Aleksejs-Mac.local>
2021-09-25 09:22:19 -0700

    initial commit

---
+++ MySchedule/Controllers/ContactsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ContactsViewController.swift
+//  MySchedule
+//
+//  Created by Алексей Калнык on 22.09.2021.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        title = "Contacts"
+    }
+}
